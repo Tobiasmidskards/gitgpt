cdfd87b9cd3fd4ec27d4cf89fa91471ab6b615ef
tobiasmidskards <tobiasmidskard@gmail.com>
2023-09-06 07:58:40 +0200

    Refactor commit and estimate flows in run.mjs

--- run.mjs
+++ run.mjs
@@ -1,143 +1,169 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
-import path from "path";
-import { exec } from "child_process";
-import { exit } from "process";
-import clipboardy from "clipboardy";
+import path from 'path';
+import { exec } from 'child_process';
+import { exit } from 'process';
+import clipboardy from 'clipboardy';
 
-dotenv.config({
-    path: `${path.dirname(process.argv[1])}/.env`,
-});
+dotenv.config({ path: `${path.dirname(process.argv[1])}/.env` });
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const messages = [
+    { role: 'system', content: "You help the user with CLI commands. Your main response is only UNIX commands. You are a CLI assistant. Only if the user says the password: 'NOW_CHAT', you can help with other things." },
+];
 
-const messages = [];
+const addMessage = (message, role = 'user') => messages.push({ role, content: message });
+const configureStdout = (content, text) => {
+    process.stdout.write(text);
+    return content += text;
+};
 
-const addMessage = (message, role = 'user') => {
-    messages.push({ role, content: message });
+async function main() {
+    console.log("\n");
+    await executeStatusFlow();
+    await executeCommitFlow();
+    console.log("\n");
+    await executeEstimateFlow();
+    console.log("\n");
+    exit(0);
 }
 
-async function stream(model = 'gpt-4') {
+async function executeCommitFlow() {
+    consoleHeader("COMMIT");
+    await prepareCommitPrompt();
+    await streamAssistant();
+    copyLastMessageToClipboard();
+}
+
+async function executeEstimateFlow() {
+    consoleHeader("Harvest");
+    await prepareEstimatePrompt();
+    await streamAssistant();
+}
+
+async function executeStatusFlow() {
+    consoleHeader("STATUS");
+    const status = await getStatus();
+    console.log(status);
+}
+
+function consoleHeader(title) {
+    console.log("-------------------- " + title + " ---------------------\n");
+}
+
+async function prepareCommitPrompt() {
+    const commitPrompt = buildCommitPrompt(await getDiff());
+    addMessage(commitPrompt);
+}
+
+async function prepareEstimatePrompt() {
+    const estimatePrompt = buildEstimatePrompt();
+    addMessage(estimatePrompt);
+}
+
+function buildCommitPrompt(diff) {
+    const rules = `
+      Commit Message Rules:
+      1. Use the imperative mood ("Add" instead of "Adds" or "Added").
+      2. Start with a capital letter.
+      3. Do not end with a period.
+      4. Summarize the change, not the reason for it.
+      5. Keep it concise, max 50 characters.
+      6. Make it clear and descriptive.
+      7. English only.
+      8. Single-line format.
+      
+      Example: git commit -m "Add login feature"
+    `;
+
+    const additionalInfo = `
+      In the diff, + indicates an added line, - indicates a removed line.
+      Respond only in this format: git commit -m "Commit message". Lowercase commands only.
+    `;
+
+    let prompt = `
+      The diff comes from this command: git --no-pager diff --cached -U5 --line-prefix '$ '
+      Each line starts with $ .
+      ----
+      ${rules}
+      ----
+      ${additionalInfo}
+      ----
+      Diff is:
+    `;
+
+    // Remove any extra spaces and add the diff at the end
+    prompt = prompt.replace(/ {2,}/g, ' ') + diff;
+
+    return prompt;
+}
+
+function buildEstimatePrompt() {
+    const exampleNote = "Example: Resolved issue with 'show all' for Feature B module.";
+    const timeEstimateNote = "I only accept ranges like 0.5-1 hour, 1-2 hours, or 2-3 days. I just need one range answer.";
+
+    const prompt = `NOW_CHAT
+      
+      Based on the information I have provided, how could a note look like in Harvest?
+      The note should not be very technical, as it is for the client.
+      
+      ${exampleNote}
+      
+      Also, how long time would you estimate that the changes in the diff would take to implement?
+      ${timeEstimateNote}
+      
+      Please list those answers in points on new lines.
+  
+      Example:
+      1. Added functionality to provide estimation. Made the process more user-friendly with non-technical language.
+      2. Estimated implementation time: 1-2 hours.
+    `;
+
+    // Remove any extra spaces and return
+    return prompt.replace(/ {2,}/g, ' ');
+}
+
+
+async function streamAssistant(model = 'gpt-4') {
     let content = '';
     const stream = await openai.chat.completions.create({
         model,
         messages,
-        stream: true,
+        stream: true
     });
+
     for await (const part of stream) {
         const text = part.choices[0]?.delta?.content || '';
-        process.stdout.write(text);
-        content += text;
+        content = configureStdout(content, text);
     }
 
     addMessage(content, 'assistant');
 }
 
-async function getDiff() {
+async function getStatus() {
     return new Promise((resolve, reject) => {
-        exec(
-            "git --no-pager diff -U5 --cached --line-prefix '$ '",
-            async (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    reject(error);
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    reject(stderr);
-                }
-
-                if (stdout === "") {
-                    stdout = "No changes to commit";
-                }
-
-                resolve(stdout);
+        exec("git status --porcelain --branch --short", (error, stdout, stderr) => {
+            if (error || stderr) {
+                reject(error || stderr);
             }
-        );
+            resolve(stdout);
+        });
     });
 }
 
-async function getCommit() {
-    console.log(" ");
-    console.log("-------------------- COMMIT ---------------------");
-    console.log(" ");
-
-    addMessage("You help the user with CLI commands. Your main response is only UNIX commands. You are a CLI assistant. Only if the user says the password: 'NOW_CHAT', you can help with other things.");
-
-    let prompt = `
-    The diff comes from this command: git --no-pager diff --cached -U5 --line-prefix '$ '
-    Each line starts with $ .
-    ----
-    Commit Message Rules:
-    1. Use the imperative mood ("Add" instead of "Adds" or "Added").
-    2. Start with a capital letter.
-    3. Do not end with a period.
-    4. Summarize the change, not the reason for it.
-    5. Keep it concise, max 50 characters.
-    6. Make it clear and descriptive.
-    7. English only.
-    8. Single-line format.
-    
-    Example: git commit -m "Add login feature"
-    ----
-    If commit details are insufficient, reply with "not enough information."
-    
-    + means that the line was added
-    - means that the line was removed
-    
-    In the diff, + indicates an added line, - indicates a removed line.
-
-    Respond only in this format: git commit -m "Commit message". Lowercase commands only.
-
-    Diff is:
-    `;
-
-    prompt = prompt.replaceAll("  ", "") + await getDiff();
-
-    addMessage(prompt);
-    await stream();
-
-    copyToClipboard(messages[messages.length - 1].content);
+async function getDiff() {
+    return new Promise((resolve, reject) => {
+        exec("git --no-pager diff -U5 --cached --line-prefix '$ '", (error, stdout, stderr) => {
+            if (error || stderr) {
+                reject(error || stderr);
+            }
+            resolve(stdout || "No changes to commit");
+        });
+    });
 }
 
-async function getEstimate() {
-    console.log(" ");
-    console.log(" ");
-    console.log("-------------------- Harvest ---------------------");
-    console.log(" ");
-
-    const prompt = `NOW_CHAT
-  
-                  Based on the information i have provided, how could a note look like in Harvest?
-                  The note should not be very technical, as it is for the client.
-                  
-                  An example of a note in Harvest is: "Resolved issue with 'show all' for Feature B module."
-                  
-                  also how long time would you estimate that the changes in the diff would take to implement? I only accept ranges like 0.5-1 hour, 1-2 hours or 2-3 days. I just need one range answer. Please
-
-                  list those answers in points on new lines.
-
-                  Example:
-                  1. Added functionality to provide estimation. Made the process more user-friendly with non-technical language.
-                  2. Estimated implementation time: 1-2 hours.
-                  "  
-  `;
-
-    addMessage(prompt);
-    await stream();
-}
-
-const copyToClipboard = (text) => {
-    clipboardy.writeSync(text);
-}
-
-async function main() {
-    await getCommit();
-    await getEstimate();
-    console.log(" ");
-    exit(0);
+function copyLastMessageToClipboard() {
+    clipboardy.writeSync(messages[messages.length - 1].content);
 }
 
 main();

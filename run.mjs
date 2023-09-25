2dab2fbe26b24f41d9d4539d13b538a1dfff7ec5
tobiasmidskards <tobiasmidskard@gmail.com>
2023-09-25 10:44:39 +0200

    Update consoleInfo parameters in runQueue function

--- run.mjs
+++ run.mjs
@@ -40,7 +40,7 @@ async function runQueue() {
     
     if (queue.length > 0) {
         const { command, args } = queue.shift();
-        consoleInfo("Running: [" + (command.name || command) + "]", 1);
+        consoleInfo("Running: [" + (command.name || command) + "]", 1, 1, true);
         await command(args);
         await runQueue();
         return;

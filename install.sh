375214924df0c2cbfac8c7dae997dadf24e9f303
tobiasmidskards <tobiasmidskard@gmail.com>
2023-11-10 11:54:30 +0100

    Improve install script with user feedback and fail-safes

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# Clone the repository
-git clone https://github.com/tobiasmidskards/gitgpt.git
-cd gitgpt
+set -e
 
-# Prompt for the OpenAI API key
+echo "Cloning the repository..."
+git clone https://github.com/tobiasmidskards/gitgpt.git
+echo "Cloned the repository."
+
+echo "Changing directory to gitgpt..."
+cd gitgpt
+echo "Changed directory to gitgpt."
+
+echo "Asking for your OpenAI API key..."
 read -p "Enter your OpenAI API key: " openai_key
+echo "Got your OpenAI API key."
 
 # Create or update the .env file with the provided key
 echo "OPENAI_API_KEY=$openai_key" > .env
+echo "Created or updated the .env file."
 
+
+echo "Installing the dependencies..."
 # Install the dependencies
-npm install -g
+npm install
+echo "Installed the dependencies."
